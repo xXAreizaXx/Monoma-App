e4c7aee938b0008c7095a15c5d0a19fcd5446c77
xXAreizaXx <jorgeareiza2001@gmail.com>
2023-03-25 16:02:06 -0500

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+// NextJS
 import type { AppProps } from 'next/app'
+
+// Styles
+import '@styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,4 @@
+// NextJS
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
